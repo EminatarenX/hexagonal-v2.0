ad65d89f8728a93dd50f46cc10eeb53c49821f13
Eminataren <eminataren2002@gmail.com>
2024-02-04 01:18:36 -0600

    anadidendo eventos de socket io

--- src/socket/infraestructure/SocketRepository.ts
+++ src/socket/infraestructure/SocketRepository.ts
@@ -1,13 +1,15 @@
 import { ISocketRepository } from "../domain/ISocketRepository";
-import { Server } from "socket.io";
+import { Server, Socket } from "socket.io";
 
 export class SocketRepository implements ISocketRepository {
     constructor(private readonly io: Server){}
     connect(): void {
         this.io.on('connection', (socket) => {
-            console.log('connected ' + socket.id)
-
-            
+            console.log('new connection')
+            socket.on('disconnect', () => {
+                console.log('user disconnected')
+            })
         })
     }
+    
 }
